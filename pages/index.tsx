91620af80169e858a9426a4a9870c57f23b890fc
richarepo <richa@repozitory.com>
2022-10-29 13:07:36 +0530

    Add metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 /** @format */
 
+import Head from "next/head";
 import ProductDetail from "../components/products/ProductDetails";
 import styles from "../styles/Home.module.css";
 
 export default function Home({ product }: any) {
+   const { name, description, offer, productImage } = product;
   return (
     <div className={styles.container}>
+      <Head>
+        <title>{name}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content="Sweater, winter, jacket, black jacket" />
+        <meta name="author" content="Sumit lal verma" />
+      </Head>
       <ProductDetail product={product} />
     </div>
   );
